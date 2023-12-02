5aef42f10e79a874c95f104957ae6b679e48390b
Muhammad Aqib Khan <raiaqibkhan545@gmail.com>
2023-12-02 23:15:14 +0500

    Blogs Assignmets

---
+++ app/(components)/footer/footer.tsx
@@ -0,0 +1,89 @@
+export default function Footer(){
+    return(
+        <div>
+            		{/* <!-- footer --> */}
+	<div className="footer-area">
+		<div className="container">
+			<div className="row">
+				<div className="col-lg-3 col-md-6">
+					<div className="footer-box about-widget">
+						<h2 className="widget-title">About us</h2>
+						<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam cupiditate omnis mollitia recusandae </p>
+					</div>
+				</div>
+				<div className="col-lg-3 col-md-6">
+					<div className="footer-box get-in-touch">
+					
+					</div>
+				</div>
+				<div className="col-lg-3 col-md-6">
+					<div className="footer-box pages">
+						<h2 className="widget-title">Pages</h2>
+						<ul>
+							<li><a href="index.html">Home</a></li>
+							<li><a href="about.html">About</a></li>
+							<li><a href="services.html">Shop</a></li>
+							<li><a href="news.html">News</a></li>
+							<li><a href="contact.html">Contact</a></li>
+						</ul>
+					</div>
+				</div>
+				<div className="col-lg-3 col-md-6">
+					<div className="footer-box subscribe">
+						<h2 className="widget-title">Subscribe</h2>
+						<p>Subscribe to our mailing list to get the latest updates.</p>
+						<form action="index.html">
+							
+							<button type="submit"><i className="fas fa-paper-plane"></i></button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	{/* <!-- end footer --> */}
+	{/* <!-- copyright --> */}
+	<div className="copyright">
+		<div className="container">
+			<div className="row">
+				<div className="col-lg-6 col-md-12">
+					<p>Copyrights &copy; 2019 - <a href="https://imransdesign.com/">M.Aqib Khan</a>,  All Rights Reserved.</p>
+				</div>
+				<div className="col-lg-6 text-right col-md-12">
+					<div className="social-icons">
+						<ul>
+							<li><a href="#" target="_blank"><i className="fab fa-facebook-f"></i></a></li>
+							<li><a href="#" target="_blank"><i className="fab fa-twitter"></i></a></li>
+							<li><a href="#" target="_blank"><i className="fab fa-instagram"></i></a></li>
+							<li><a href="#" target="_blank"><i className="fab fa-linkedin"></i></a></li>
+							<li><a href="#" target="_blank"><i className="fab fa-dribbble"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+{/* !-- jquery --> */} */
+<script src="/assets/js/jquery-1.11.3.min.js"></script>
+{/* <!-- bootstrap --> */}
+<script src="/assets/bootstrap/js/bootstrap.min.js"></script>
+{/* <!-- count down --> */}
+<script src="/assets/js/jquery.countdown.js"></script>
+{/* <!-- isotope --> */}
+<script src="/assets/js/jquery.isotope-3.0.6.min.js"></script>
+{/* <!-- waypoints --> */}
+<script src="/assets/js/waypoints.js"></script>
+{/* <!-- owl carousel --> */}
+<script src="/assets/js/owl.carousel.min.js"></script>
+{/* <!-- magnific popup --> */}
+<script src="/assets/js/jquery.magnific-popup.min.js"></script>
+{/* <!-- mean menu --> */}
+<script src="/assets/js/jquery.meanmenu.min.js"></script>
+{/* <!-- sticker js --> */}
+<script src="/assets/js/sticker.js"></script>
+{/* <!-- main js --> */}
+<script src="/assets/js/main.js"></script>
+	
+        </div>
+    )
+}
---
+++ app/(components)/footer/navbar/navbar.tsx
@@ -0,0 +1,49 @@
+export default function Navbar() {
+    return(
+        <div>
+            			<nav className="main-menu">
+							<ul>
+								<li className="current-list-item"><a href="#">Home</a>
+									<ul className="sub-menu">
+										<li><a href="index.html">Static Home</a></li>
+										<li><a href="index_2.html">Slider Home</a></li>
+									</ul>
+								</li>
+								<li><a href="about.html">About</a></li>
+								<li><a href="#">Pages</a>
+									<ul className="sub-menu">
+										<li><a href="404.html">404 page</a></li>
+										<li><a href="about.html">About</a></li>
+										<li><a href="cart.html">Cart</a></li>
+										<li><a href="checkout.html">Check Out</a></li>
+										<li><a href="contact.html">Contact</a></li>
+										<li><a href="news.html">News</a></li>
+										<li><a href="shop.html">Shop</a></li>
+									</ul>
+								</li>
+								<li><a href="news.html">News</a>
+									<ul className="sub-menu">
+										<li><a href="news.html">News</a></li>
+										<li><a href="single-news.html">Single News</a></li>
+									</ul>
+								</li>
+								<li><a href="contact.html">Contact</a></li>
+								<li><a href="shop.html">Shop</a>
+									<ul className="sub-menu">
+										<li><a href="shop.html">Shop</a></li>
+										<li><a href="checkout.html">Check Out</a></li>
+										<li><a href="single-product.html">Single Product</a></li>
+										<li><a href="cart.html">Cart</a></li>
+									</ul>
+								</li>
+								<li>
+									<div className="header-icons">
+										<a className="shopping-cart" href="cart.html"><i className="fas fa-shopping-cart"></i></a>
+										<a className="mobile-hide search-bar-icon" href="#"><i className="fas fa-search"></i></a>
+									</div>
+								</li>
+							</ul>
+						</nav>
+        </div>
+    )
+}
